6717cb72de6e9252faac7953f32c850fefd21918
IngridHer004 <158096090+IngridHer004@users.noreply.github.com>
2024-03-08 15:35:20 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: 
+true}));
+
+
+app.listen(4000, ()=> {
+    console.log("Ingrid Janeth Hernandez Salazar Funciona :)")
+});
